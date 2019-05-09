8c386729c8f249c2b443f0f16e108850a8a76c82
JuliusHaring <julius.h9191@googlemail.com>
2019-05-09 16:27:55 +0200

    added chunking and POS tagging

--- Python/GodsBehaviour.py
+++ Python/GodsBehaviour.py
@@ -1,23 +1,37 @@
 from TopicExtractor import TopicExtractor
 from nltk import word_tokenize
 from nltk import pos_tag as pos_tag
+from nltk.chunk import RegexpParser
+from nltk.stem import WordNetLemmatizer
 
 te = TopicExtractor()
 
-testaments = ["",""]
 books = te.getFullTextBooks()
+tagged = [[],[]]
 
 for i in range(len(books)):
     book = books[i]
     for chapter in book:
         for sentence in chapter:
             if i < 39:
-                testaments[0] = testaments[0] + sentence + "\n "
+                tagged[0].append(pos_tag(word_tokenize(sentence)))
             else:
-                testaments[1] = testaments[1] + sentence + "\n "
+                tagged[1].append(pos_tag(word_tokenize(sentence)))
 
-tagged = []
-tagged.append(pos_tag(word_tokenize(testaments[0])))
-tagged.append(pos_tag(word_tokenize(testaments[1])))
 
-print(tagged)
+grammar = "Chunk: {<NNP><VB.?>}"
+parser = RegexpParser(grammar)
+
+verbs = [[],[]]
+
+for t in range(len(tagged)):
+    for sentence in tagged[t]:
+        temp = parser.parse(sentence)
+        for subtree in temp.subtrees(filter=lambda t: t.label() == 'Chunk'):
+                if(str(subtree[0][0]).lower() == "god"):
+                    verb = subtree[1][0]
+                    verb = WordNetLemmatizer().lemmatize(verb,'v')
+                    verbs[t].append(verb)
+
+
+p = verbs
