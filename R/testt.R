42ab0bd8c9132568b5feae466dbbfa195fc9b07a
Stephan Delhey <stephan@stephans-mbp.wireless.unimaas.local>
2019-05-09 17:27:43 +0200

    streamgraph example

---
+++ R/testt.R
@@ -0,0 +1,12 @@
+# Library
+library(streamgraph)
+
+# Create data:
+year=rep(seq(1990,2016) , each=10)
+name=rep(letters[1:10] , 27)
+value=sample( seq(0,1,0.0001) , length(year))
+data=data.frame(year, name, value)
+
+# Stream graph with a legend
+streamgraph(data, key="name", value="value", date="year" )%>%
+  sg_legend(show=TRUE, label="names: ")
